a8a58e4efaf220d7587d8da01514e3354d6c1b78
Tom Ray <tomwray13@gmail.com>
2022-07-22 10:50:40 +0100

    Inject cache manager example

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,14 +1,29 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class PokemonService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
+  ) {}
 
   async getPokemon(id: number): Promise<string> {
+    // check if data is in cache:
+    const cachedData = await this.cacheService.get<{ name: string }>(
+      id.toString(),
+    );
+    if (cachedData) {
+      console.log(`Getting data from cache!`);
+      return `${cachedData.name}`;
+    }
+
+    // if not, call API and set the cache:
     const { data } = await this.httpService.axiosRef.get(
       `https://pokeapi.co/api/v2/pokemon/${id}`,
     );
+    await this.cacheService.set(id.toString(), data);
     return await `${data.name}`;
   }
 }
