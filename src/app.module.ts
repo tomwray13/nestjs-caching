1909fb0a8c4c59dc4ae60be16ee79e2900da8c60
Tom Ray <tomwray13@gmail.com>
2022-07-23 13:35:43 +0100

    Use local redis server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [CacheModule.register({ isGlobal: true }), PokemonModule],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+    PokemonModule,
+  ],
 })
 export class AppModule {}
