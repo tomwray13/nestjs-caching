1c5aa7945c773dda1b2a044772e9a576fa0071af
Tom Ray <tomwray13@gmail.com>
2022-07-23 13:56:44 +0100

    Add environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
 import * as redisStore from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CacheModule.register({
       isGlobal: true,
       store: redisStore,
-      host: 'localhost',
-      port: 6379,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+      no_ready_check: true,
     }),
     PokemonModule,
   ],
