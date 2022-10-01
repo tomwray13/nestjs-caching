957aafb455a29c2120a5301f6ddc1bfa1640198a
Robert Bakker <rbakker@rocsys.com>
2022-10-01 08:52:59 +0200

    fix tests

--- src/pokemon/pokemon.controller.spec.ts
+++ src/pokemon/pokemon.controller.spec.ts
@@ -1,12 +1,17 @@
+import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonController } from './pokemon.controller';
+import { PokemonService } from './pokemon.service';
 
 describe('PokemonController', () => {
   let controller: PokemonController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, CacheModule.register({})],
       controllers: [PokemonController],
+      providers: [PokemonService],
     }).compile();
 
     controller = module.get<PokemonController>(PokemonController);
